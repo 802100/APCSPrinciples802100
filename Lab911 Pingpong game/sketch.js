125f1ff70bf9862910f3b6f4ccca54e648394306
Evangeline Viray <eviray@WS-ACADBUS.EDU>
2019-09-23 08:51:09 -0700

    Lab911 PingPong Game
    finally settup the title screen and play screen. still need to configure the end screen and fix the is colliding.

--- Lab911 Pingpong game/sketch.js
+++ Lab911 Pingpong game/sketch.js
@@ -4,6 +4,9 @@
 var balls = []
 var paddle;
 var gameState = 1;
+var difficulty = 0;
+var n = 5;
+var lives = 10;
 
 
 //  The setup function function is called once when your program begins
@@ -11,7 +14,7 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   // background(5, 5, 5);
-  loadBalls(10);
+  loadBalls(n);
   loadPaddles();
 
 
@@ -28,18 +31,56 @@ function draw() {
     endGame();
   }
 //edit out when finished fixing isCollinding
-  runBalls();
-  runPaddles();
+  // runBalls();
+  // runPaddles();
 
 
 }
 //sart Game screen
 function startGame(){
   background(50,50,50);
+  //title
+  textSize(50);
+  fill(255);
+  text("PaddleBall",270,200);
+  //dificulty setting
+  textSize(20);
+  fill(0,50,250);
+  rect(200,600,70,40);
+  fill(255);
+  text("easy",215,625);
+  fill(250,0,250);
+  rect(350,600,70,40);
+  fill(255);
+  text("med",365,625);
+  fill(255,0,0);
+  rect(500,600,70,40);
+  fill(255);
+  text("hard",515,625);
+  //dificulty
+  if(mouseIsPressed &&
+    mouseX>200 && mouseX<280 &&
+    mouseY>600 && mouseY<640){
+      gameState = 2;
+    }
+  if(mouseIsPressed &&
+    mouseX>350 && mouseX<430 &&
+    mouseY>600 && mouseY<640){
+      gameState = 2;
+    }
+  if(mouseIsPressed &&
+    mouseX>500 && mouseX<580 &&
+    mouseY>600 && mouseY<640){
+      gameState = 2;
+    }
+
 }
 //play game screen
 function playGame(){
   background(0);
+  textSize(10);
+  fill(255);
+  text("lives = " + lives,20,20);
   runBalls();
   runPaddles();
 }
