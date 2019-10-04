03ef2ec82960470225bb2e2de3115bbf79afe2b3
Evangeline Viray <eviray@WS-ACADBUS.EDU>
2019-10-04 08:45:29 -0700

    pgame v2

---
+++ Lab911 Pingpong gamev2/Ball.js
@@ -0,0 +1,80 @@
+//start ball class (8/20)
+//rotation
+class Ball{
+  constructor(x,y,dx,dy){
+    this.loc = createVector(x,y);
+    this.vel = createVector(dx,dy);
+    this.acc = createVector(0,0.1);
+    this.w = 20;
+    this.clr = color(random(255),random(255),random(255));
+
+
+    }
+
+  run(){
+    this.checkEdges();
+    this.update();
+    this.render();
+
+
+  }
+  //is the ball cliiding with the paddle?
+  isColliding(){
+    if(this.loc.x>paddle.loc.x &&
+      this.loc.x<paddle.loc.x + paddle.w &&
+      this.loc.y>paddle.loc.y &&
+      this.loc.y<paddle.loc.y + paddle.h){
+        return true;
+      }else{
+        return false;
+      }
+
+  }
+
+
+  checkEdges(){
+    //if colliding with paddle and velocity is negative
+    for(var i = balls.length -1; i >= 0; i--){
+      if(balls[i].isColliding() && balls[i].vel.y < 0){
+        console.log("collision");
+        lives = lives - 1;
+        loadBalls(b + 3);
+        clear();
+      }
+    }
+    //if colliding with paddle
+    for(var i = balls.length -1; i >= 0; i--){
+      if(balls[i].isColliding()){
+        score = score + 20;
+        balls.splice(i,1)
+      }
+    }
+    //if hitting edge of screen
+      if(this.loc.x < 0){
+        this.vel.x = -this.vel.x;
+      }
+      if(this.loc.x > width){
+        this.vel.x = -this.vel.x;
+      }
+      if(this.loc.y <0){
+        this.vel.y = -this.vel.y;
+      }
+      if(this.loc.y > height){
+        this.vel.y = -this.vel.y;
+      }
+
+
+
+  }
+
+  update(){
+
+
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+  }
+  render(){
+    fill(this.clr);
+    ellipse(this.loc.x,this.loc.y,this.w,this.w);
+  }
+}// end ball class
