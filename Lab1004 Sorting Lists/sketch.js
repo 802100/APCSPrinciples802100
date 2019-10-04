99fda517f713721a1c7bd2ceb4f281e0d7178c36
Evangeline Viray <eviray@WS-ACADBUS.EDU>
2019-10-04 08:50:06 -0700

    Lab1004 Sorting Lists

---
+++ Lab1004 Sorting Lists/sketch.js
@@ -0,0 +1,23 @@
+//  Evangeline Viray
+// 1004
+//  This is a comment
+//  The setup function function is called once when your program begins
+var list = [9,11,5,3,6];
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+
+
+
+}
