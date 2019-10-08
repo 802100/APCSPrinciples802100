2f56a69e1cfb0bdfc41e6e9052d9ddf38848885e
802100 <54591147+802100@users.noreply.github.com>
2019-10-08 08:51:04 -0700

    LAb1007Checkerboard

--- Lab1007CheckerBaord/StartCode/CheckerBoard/sketch.js
+++ Lab1007CheckerBaord/StartCode/CheckerBoard/sketch.js
@@ -3,23 +3,24 @@
 //
 //global variables
 var squares = [];
-
+var x = 0;
+var y = 0;
 
 // put setup code here
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(235);
-  loadSquares(1);
+  loadSquares(2);
   // traverse array and run each squares
   for(var i = 0; i < squares.length; i++){
-    squares[i].render();
+    squares[i].run();
   }
 }
 
 function loadSquares(n){
   for(var i = 0; i < n; i ++){
-    squares[i] = new Square(0,0,100);
+    squares[i] = new Square(x,y,100);
   }
 
 }
