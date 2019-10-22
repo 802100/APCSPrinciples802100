7cf03004010f3af6a769714646b30c631e5969a5
Evangeline Viray <eviray@WS-ACADBUS.EDU>
2019-10-22 08:46:42 -0700

    Lab1021 SelectionSort
    got all of the swap code down. Also finished the ranNum function to a certain extent. Just need to add in the timer function.

--- Lab1017 InsertionSort/sketch.js
+++ Lab1017 InsertionSort/sketch.js
@@ -1,25 +1,28 @@
 //  Evangeline Viray
-// 1004
+// 1017
 //  This is a comment
 //  The setup function function is called once when your program begins
 var list = [];
 var temp;
+var swaps = 0;
+var comps = 0;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  ranNum(10);
+  ranNum(9);
   console.log(list);
-  BubbleSort(list.length);
+  InsertionSort(list.length);
 
 }
 
 //random number function
 function ranNum(n){
   for(var i = 0; i < n; i ++){
-    for(var j = 0; j = n+j; j++){
+    for(var j = 0; j < n+j+1; j++){
       list[n] = round(random(1,200));
+      n = n-1;
     }
   }
 
@@ -34,18 +37,22 @@ function Swap(x,y){
 
 }
 
-//start BubbleSort
-function BubbleSort(n){
+//start InsertionSort
+function InsertionSort(n){
   for(var i = 1; i < n; i++){
     for(var j = i; j > 0; j--){
+      comps = comps+1;
       if(list[j] < list[j-1]){
         Swap(j, j-1);
+        swaps = swaps+1;
       }
     }
   }
   console.log(list);
+  console.log("compares = "+ comps);
+  console.log("swaps = "+ swaps);
 
-}//end BubbleSort
+}//end InsertionSort
 //  The draw function is called @ 30 fps
 function draw() {
 
--- Lab1021 SelectionSort/sketch.js
+++ Lab1021 SelectionSort/sketch.js
@@ -1,16 +1,31 @@
 //  Evangeline Viray
-// 1004
+// 1021
 //  This is a comment
 //  The setup function function is called once when your program begins
-var list = [9,11,5,3,6];
+var list = [];
 var temp;
+var swaps = 0;
+var comps = 0;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
+  ranNum(9);
   console.log(list);
-  BubbleSort(list.length);
+  SelectionSort(list.length);
+
+}
+
+//ranNum
+function ranNum(n){
+  for(var i = 0; i < n; i ++){
+    for(var j = 0; j < n+j+1; j++){
+      list[n] = round(random(1,200));
+      n = n-1;
+    }
+  }
+
 
 }
 //start swap
@@ -22,18 +37,24 @@ function Swap(x,y){
 
 }
 
-//start BubbleSort
-function BubbleSort(n){
+//start SelectionSort
+function SelectionSort(n){
   for(var i = 0; i < n-1; i++){
-    for(var j = 0; j < n-1-i; j++){
-      if(list[j+1] < list[j]){
-        Swap(i, j);
+    var index = i;
+    for(var j = i+1; j < n; j++){
+      comps = comps+1;
+      if(list[j] < list[index]){
+        index = j;
       }
     }
+    Swap(index, i);
+    swaps = swaps+1;
   }
   console.log(list);
+  console.log("compares = "+ comps);
+  console.log("swaps = "+ swaps);
 
-}//end BubbleSort
+}//end SelectionSort
 //  The draw function is called @ 30 fps
 function draw() {
 
