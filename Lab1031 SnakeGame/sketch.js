e2342d9f16974b9041bd88ea9e960d930a1aaa40
802100 <54591147+802100@users.noreply.github.com>
2019-11-06 21:42:45 -0800

    Lab1031 SnakeGame
    I added some comments

--- Lab1031 SnakeGame/sketch.js
+++ Lab1031 SnakeGame/sketch.js
@@ -7,8 +7,6 @@ var food;
 var snake;
 var rowH = 800/20;
 var colW = 800/20;
-var s = 1;
-var p = 0;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -31,21 +29,21 @@ function draw() {
   runSnake();
 
 }
-
+// load food in random location
 function loadFood(){
     food = new Food(colW*round(random(20)),rowH*round(random(20)));
 
   }
-
+// render food in random location
 function runFood(){
   food.run();
   }
-
+//load snake head
 function loadSnake(){
   snake = new Snake(colW*10,rowH*10,0,0);
 }
 
-
+// run snake head
 function runSnake(){
   snake.run();
 }
